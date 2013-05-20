0b20c51a4a7ff639d9f368f519935844d181d187
Stephen Bailey <stephen3200@yahoo.com>
2013-05-20 23:53:00 +0100

    Removal of old files

--- 2D cellular Automata/Fixed Value Cellular Automata/CAUtils.h
+++ 2D cellular Automata/Fixed Value Cellular Automata/CAUtils.h
@@ -5,3 +5,4 @@
 
 void printrow (int PopArray[ArraySize][ArraySize]);
 void nextstate(int TempPop[ArraySize][ArraySize], int PopArray[ArraySize][ArraySize], double K1, double K2, double K3, double K4);
+void nextstate_Conway(int TempPop[ArraySize][ArraySize], int PopArray[ArraySize][ArraySize]);
